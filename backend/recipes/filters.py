5ff0f8879951b1102082a71eb917015ca96bc857
Валерий Юрченко <valerayurchenko14@yandex.ru>
2022-10-07 03:08:34 +0500

    favorite fix10

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,25 +1,33 @@
-from django_filters import rest_framework as filters
+import django_filters as filters
 
 from .models import Recipe, Tag
 
 
 class RecipeFilter(filters.FilterSet):
-    author = filters.NumberFilter(
-        field_name='author__id',
-        lookup_expr='exact'
-    )
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
+        queryset=Tag.objects.all(),
         to_field_name='slug',
-        queryset=Tag.objects.all()
     )
     is_favorited = filters.BooleanFilter(
-        field_name='is_favorited'
+        method='get_is_favorited'
     )
     is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart'
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags', 'is_favorited', 'is_in_shopping_cart']
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(favorites__user=user)
+        return Recipe.objects.all()
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(purchases__user=user)
+        return Recipe.objects.all()
