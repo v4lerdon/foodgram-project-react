2996ae2ded76edeb652130eecd6fc9e57cb3df6c
Валерий Юрченко <valerayurchenko14@yandex.ru>
2022-10-07 03:02:25 +0500

    favorite fix7

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -4,9 +4,9 @@ from .models import Recipe
 
 
 class RecipeFilter(rest_framework.FilterSet):
-    tags = rest_framework.AllValuesMultipleFilter(
-        field_name='tags__slug',
-    )
+    # tags = rest_framework.AllValuesMultipleFilter(
+    #     field_name='tags__slug',
+    # )
     is_favorited = rest_framework.BooleanFilter(
         method='get_is_favorited'
     )
@@ -16,7 +16,7 @@ class RecipeFilter(rest_framework.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('author', 'is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
         user = self.request.user
