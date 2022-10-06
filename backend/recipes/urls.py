da2d82d67628cbefa95d2a079e5c63d940ba990a
Валерий Юрченко <valerayurchenko14@yandex.ru>
2022-10-07 02:02:38 +0500

    favorite fix1

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from .views import (DownloadShoppingCart, FavoriteView, IngredientViewSet,
                     RecipeViewSet, ShoppingCartView, TagViewSet)
 
